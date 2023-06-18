b36d5c130c02bb449c06dacc66751d3b6219df31
Durganshu Mishra <60805696+Durganshu@users.noreply.github.com>
2023-06-18 18:42:43 +0200

    Update build_benchmark.sh

--- stl_algorithms/build_benchmark.sh
+++ stl_algorithms/build_benchmark.sh
@@ -1,1 +1,3 @@
 g++ transform_performance.cpp -std=c++20 -O3 -isystem benchmark/include  -Lbenchmark/build/src -lbenchmark -lpthread -ltbb -o transform_benchmark
+
+g++ sort_performance.cpp -std=c++20 -O3 -isystem benchmark/include  -Lbenchmark/build/src -lbenchmark -lpthread -ltbb -o sort_benchmark
